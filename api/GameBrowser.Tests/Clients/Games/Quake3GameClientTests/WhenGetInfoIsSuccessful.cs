33407320dc4826727acb2754016f3442e2b7609e
Jon Cain <jonifen@users.noreply.github.com>
2020-08-12 23:45:41 +0100

    Add tests for Quake3GameClient

---
+++ api/GameBrowser.Tests/Clients/Games/Quake3GameClientTests/WhenGetInfoIsSuccessful.cs
@@ -0,0 +1,56 @@
+﻿using GameBrowser.Clients.Games;
+using GameBrowser.Clients.Protocols;
+using GameBrowser.Models;
+using Moq;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace GameBrowser.Tests.Clients.Games.Quake3GameClientTests
+{
+    public class WhenGetInfoIsSuccessful
+    {
+        private Mock<IQuake3Client> _protocolClient;
+        private string _ipAddress;
+        private int _port;
+        private ServerResponse _expectedResponse;
+        private Quake3GameClient _gameClient;
+        private ServerResponse _response;
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            _ipAddress = "192.168.201.201";
+            _port = 27960;
+            _expectedResponse = new ServerResponse
+            {
+                Data = "test info data",
+                Error = null,
+                Success = true
+            };
+
+            _protocolClient = new Mock<IQuake3Client>();
+            _protocolClient.Setup(c => c.GetInfo(_ipAddress, _port)).ReturnsAsync(_expectedResponse);
+
+            _gameClient = new Quake3GameClient(_protocolClient.Object);
+            _response = await _gameClient.GetInfo(_ipAddress, _port);
+        }
+
+        [Test]
+        public void ShouldReturnData()
+        {
+            Assert.That(_response.Data, Is.EqualTo(_expectedResponse.Data));
+        }
+
+        [Test]
+        public void ShouldReturnNullError()
+        {
+            Assert.That(_response.Error, Is.Null);
+        }
+
+        [Test]
+        public void ShouldReturnSuccessTrue()
+        {
+            Assert.That(_response.Success, Is.True);
+        }
+    }
+}
