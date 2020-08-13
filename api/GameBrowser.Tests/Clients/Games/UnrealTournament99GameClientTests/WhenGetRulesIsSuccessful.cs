b94330fe82d3eccc706f7ae519bc517de68688aa
Jon Cain <jonifen@users.noreply.github.com>
2020-08-13 20:37:43 +0100

    Add tests for UnrealTournament99GameClient

---
+++ api/GameBrowser.Tests/Clients/Games/UnrealTournament99GameClientTests/WhenGetRulesIsSuccessful.cs
@@ -0,0 +1,56 @@
+﻿using GameBrowser.Clients.Games;
+using GameBrowser.Clients.Protocols;
+using GameBrowser.Models;
+using Moq;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace GameBrowser.Tests.Clients.Games.UnrealTournament99GameClientTests
+{
+    public class WhenGetRulesIsSuccessful
+    {
+        private Mock<IGamespyClient> _protocolClient;
+        private string _ipAddress;
+        private int _port;
+        private ServerResponse _expectedResponse;
+        private UnrealTournament99GameClient _gameClient;
+        private ServerResponse _response;
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            _ipAddress = "192.168.201.201";
+            _port = 7778;
+            _expectedResponse = new ServerResponse
+            {
+                Data = "test info data",
+                Error = null,
+                Success = true
+            };
+
+            _protocolClient = new Mock<IGamespyClient>();
+            _protocolClient.Setup(c => c.GetRules(_ipAddress, _port)).ReturnsAsync(_expectedResponse);
+
+            _gameClient = new UnrealTournament99GameClient(_protocolClient.Object);
+            _response = await _gameClient.GetRules(_ipAddress, _port);
+        }
+
+        [Test]
+        public void ShouldReturnData()
+        {
+            Assert.That(_response.Data, Is.EqualTo(_expectedResponse.Data));
+        }
+
+        [Test]
+        public void ShouldReturnNullError()
+        {
+            Assert.That(_response.Error, Is.Null);
+        }
+
+        [Test]
+        public void ShouldReturnSuccessTrue()
+        {
+            Assert.That(_response.Success, Is.True);
+        }
+    }
+}
